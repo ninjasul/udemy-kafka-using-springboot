50a2e72a1c37c7429989934aaee79a3f28611a1b
박동엽 <ninjasul@onestorecorp.com>
2020-09-28 15:22:00 +0900

    동기 방식의 메시지 전송 코드 작성

--- library-events-producer/src/main/java/com/udemy/kafka/library/events/producer/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/udemy/kafka/library/events/producer/controller/LibraryEventsController.java
@@ -3,14 +3,19 @@ package com.udemy.kafka.library.events.producer.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.udemy.kafka.library.events.producer.domain.LibraryEvent;
 import com.udemy.kafka.library.events.producer.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
+@Slf4j
 public class LibraryEventsController {
     @Autowired
     private LibraryEventProducer libraryEventProducer;
@@ -18,8 +23,13 @@ public class LibraryEventsController {
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(
         @RequestBody LibraryEvent libraryEvent
-    ) throws JsonProcessingException {
-        libraryEventProducer.sendLibraryEvent(libraryEvent);
+    ) throws JsonProcessingException, ExecutionException, InterruptedException {
+        log.info("Before sendLibraryEvent()");
+        //libraryEventProducer.sendLibraryEvent(libraryEvent);
+        SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+        log.info("SendResult is {}", sendResult.toString());
+        log.info("After sendLibraryEvent()");
+
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
--- library-events-producer/src/main/java/com/udemy/kafka/library/events/producer/producer/LibraryEventProducer.java
+++ library-events-producer/src/main/java/com/udemy/kafka/library/events/producer/producer/LibraryEventProducer.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Component
 @Slf4j
 public class LibraryEventProducer {
@@ -52,5 +56,27 @@ public class LibraryEventProducer {
 
     private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
         log.info("Message Sent Successfully for the key: {} and the value is {}, partition is {}", key, value, result.getRecordMetadata().partition());
+    }
+
+    public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        SendResult<Integer, String> sendResult = null;
+
+        try {
+            // 동기 방식으로 메시지 전송. timeout은 1초
+            sendResult = kafkaTemplate.sendDefault(key, value).get(1, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException | ExecutionException e) {
+            log.error("InterruptedException/ExecutionException Sending the message and the exception is {}", e.getMessage());
+            throw e;
+        }
+        catch (Exception e) {
+            log.error("Exception Sending the message and the exception is {}", e.getMessage());
+            throw e;
+        }
+
+        return sendResult;
     }
 }
