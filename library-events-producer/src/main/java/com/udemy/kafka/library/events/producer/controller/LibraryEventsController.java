dd7b5c7c2c46a1e6a48935db3172f0f1aa5c96e0
박동엽 <ninjasul@onestorecorp.com>
2020-09-28 14:58:49 +0900

    Kafka 메시지 전송 코드 연결

--- library-events-producer/src/main/java/com/udemy/kafka/library/events/producer/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/udemy/kafka/library/events/producer/controller/LibraryEventsController.java
@@ -1,6 +1,9 @@
 package com.udemy.kafka.library.events.producer.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.udemy.kafka.library.events.producer.domain.LibraryEvent;
+import com.udemy.kafka.library.events.producer.producer.LibraryEventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LibraryEventsController {
+    @Autowired
+    private LibraryEventProducer libraryEventProducer;
 
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(
         @RequestBody LibraryEvent libraryEvent
-    ) {
-
-        // invoke kafka producer
+    ) throws JsonProcessingException {
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
